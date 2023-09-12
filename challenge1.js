e40f637942b4872e113b4946ef145e146672f6b4
liannealkhaled <liannealq@hotmail.com>
2023-09-12 18:17:39 +0300

    day 3 task

--- challenge1.js
+++ challenge1.js
@@ -3,6 +3,11 @@
  * Create a function named `printName`
  * - that just prints your name on the screen
  */
+function printName(name) {
+  console.log(name);
+}
+
+printName(`Lianne`);
 
 /**
  * Task 2:
@@ -11,6 +16,12 @@
  * - and prints the age on the screen.
  * - Age = current year - birth
  */
+
+function printAge(birth) {
+  console.log(2023 - birth);
+}
+
+printAge(1997);
 
 /**
  * Task 3:
@@ -23,9 +34,39 @@
  * -- tr: it should print `Merhaba NAME`
  */
 
+function printHello(name, language) {
+  if (language == `english`) {
+    console.log(`hello ${name}`);
+  }
+  if (language == `espanol`) {
+    console.log(`hola ${name}`);
+  }
+
+  if (language == `french`) {
+    console.log(`bonjour ${name}`);
+  }
+  if (language == `turkey`) {
+    console.log(`merhaba ${name}`);
+  }
+}
+
+printHello(`lianne`, `english`);
+printHello(`khaled`, `espanol`);
+printHello(`deema`, `french`);
+printHello(`latifa`, `turkey`);
+
 /**
  * Task 4:
  * Create a function named `printMax`
  * - that takes 2 parameters as numbers
  * - should print out the bigger number
  */
+
+function printMax(x, y) {
+  if (x > y) {
+    console.log(x);
+  } else {
+    y;
+  }
+}
+printMax(10, 5);
